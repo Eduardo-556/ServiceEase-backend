c31ba07b8c956c8efbfb28efed9e44f79bced6a7
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-10-23 00:26:50 -0300

    feat: removed AdminJs

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,14 @@ dotenv.config();
 import cors from "cors";
 import express from "express";
 import { database } from "./database";
-import { adminJs, adminJsRouter } from "./adminjs";
+
 import { router } from "./routes";
 
 const app = express();
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
-app.use(adminJs.options.rootPath, adminJsRouter);
+
 app.use(router);
 const PORT = process.env.PORT || 3000;
 

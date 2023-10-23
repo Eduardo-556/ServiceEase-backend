3241436bbd4755269177cd533e79f340ae4cbaf2
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-10-23 00:34:54 -0300

    chore: removed AdminJs's files

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,9 +2,4 @@ import * as env from "env-var";
 
 export const DATABASE_URL = env.get("DATABASE_URL").required().asString();
 
-export const ADMINJS_COOKIE_PASSWORD = env
-  .get("ADMINJS_COOKIE_PASSWORD")
-  .required()
-  .asString();
-
 export const JWT_SECRET = env.get("JWT_SECRET").required().asString();
